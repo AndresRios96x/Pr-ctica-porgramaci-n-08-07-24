49701191b3e117491d22094df9d8f39e65164d19
AndresRios96x <andresferio96@gmail.com>
2024-07-22 16:06:50 -0500

    Ejercicio 2 listo

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,7 +1,7 @@
-nombre = (input("Digite su nombre: "))
+nombre = input("Digite su nombre: ")
 Ingresos_del_empleado = float(input("¿Cuantos ingresos genero este mes: "))
-Comision = (Ingresos_del_empleado*13)
-print("Su comisión para este mes es de: {Comision}")  
+Comision = round(Ingresos_del_empleado * 0.13,2)
+print(f"Hola {nombre} Su comisión para este mes es de: {Comision}")  
 
 
 
