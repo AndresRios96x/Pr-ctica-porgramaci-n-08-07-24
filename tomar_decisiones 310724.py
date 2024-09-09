29bf943ad3435a68b7a13ff7e990c41439c9c806
AndresRios96x <andresferio96@gmail.com>
2024-09-09 17:56:31 -0500

    Control de flujo

--- tomar_decisiones 310724.py
+++ tomar_decisiones 310724.py
@@ -10,9 +10,20 @@
 #     print("No puedes conducir. Necesitas contar con una licencia.")
    
 
-# habla_ingles = True
-# sabe_python = False
 
+habla_ingles = True
+sabe_python = False
+
+if habla_ingles == True and sabe_python == True:
+    print("Cumples con los requisitos para postularte")
+elif habla_ingles != True and sabe_python != True:
+    print("Para postularte, necesitas saber programar en Python y tener conocimientos en inglés")
+elif habla_ingles != True and sabe_python == True:
+    print("Para postularte, necesitas tener conocimientos de inglés")
+elif habla_ingles == True and sabe_python == False:
+    print("Para postularte, necesitas saber programar en Python")
+    
+    
 
 
 
